bff8bc4f7182c3b64903b00f25c2dbf3f2299b9b
Erlin Maresta <90031044+erlinmareta@users.noreply.github.com>
2022-11-22 16:25:04 +0700

    first commit

---
+++ resources/views/gudang/about.blade.php
@@ -0,0 +1,5 @@
+<h1>MyVentory</h1>
+<p>MyVentory yang biasa disingkat "MYV" merupakan aplikasi management persediaan barang 
+    milik toko dalam sebuah gudang dimana didalamnya untuk mempermudah karyawan atau pemilik toko
+    mengorganisir data barang barangnya 
+</p>
